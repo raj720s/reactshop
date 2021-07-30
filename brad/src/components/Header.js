6a5d9ea8c517f97dd10712f3728c447f0e5976ed
raj720s <raj720s@raj.com>
2021-07-30 07:44:52 +0530

    brad shop

---
+++ brad/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Navbar } from "react-bootstrap";
+const Header = () => {
+  return (
+    <header>
+      <Navbar />
+    </header>
+  );
+};
+
+export default Header;
