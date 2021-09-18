1c09916aeb6a66bf9cf97624f9a5c152c55b0d10
raj720s <raj720s@raj.com>
2021-09-18 07:30:30 +0530

    final flash

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,14 +2,11 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
 const connectDb = async () => {
-  mongoose.connect(
-    "mongodb+srv://raj:bradshop@cluster0.kxuqe.mongodb.net/database?retryWrites=true&w=majority",
-    {
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    }
-  );
+  mongoose.connect(process.env.dbString, {
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  });
   var db = mongoose.connection;
   db.on("error", () => {
     console.log("error connection");
